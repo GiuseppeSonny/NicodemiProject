7f26b47f408bc7cac51b61dfb72b541060f82c3e
Giuseppe Neri <umanella23@gmail.com>
2025-02-10 17:18:41 +0100

    star code for shop

--- src/componets/Header/footer/Footer.jsx
+++ src/componets/Header/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>© 2025 My Website. All rights reserved.</p>
+      <p> © 2025 All rights reserved.</p>
     </div>
   );
 };
--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./works.module.scss";
 
 const Works = () => {
+  const [quantity, setQuantity] = useState(1);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    address: "",
+  });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <div className={styles.main}>
-      <div className={styles.workstext}>
-        <h2>My Work</h2>
-      </div>
-      <div className={styles.works}>
-        <ul>
-          <li>
-            <a href="https://example.com/project1">Project 1</a>
-          </li>
-          <li>
-            <a href="https://example.com/project2">Project 2</a>
-          </li>
-          <li>
-            <a href="https://example.com/project3">Project 3</a>
-          </li>
-        </ul>
+      {/*  <div className={styles.workstext}>
+        <h2>Prodotti</h2>
+      </div> */}
+
+      <div className={styles.product1}>
+        <h3>Olio E.V.O.</h3>
+        <p>Prezzo: €10</p>
+
+        <label>Quantità:</label>
+        <input
+          type="number"
+          min="1"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
       </div>
     </div>
   );
